ba82e6b1d9b3ecb94022025647f3b0dc1d27afe7
Bastien Penavayre <bastienPenava@gmail.com>
2017-03-11 17:40:42 -0500

    Add log

--- routes/travaux.js
+++ routes/travaux.js
@@ -12,6 +12,7 @@ router.route('/')
             var R = 6371; // Radius of the earth in km
             var dLat = toRad(lat2-lat1);
             var dLon = toRad(lon2-lon1);
+            console.log(dLat + " " + dLon);
             var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
                 Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
                 Math.sin(dLon/2) * Math.sin(dLon/2);
