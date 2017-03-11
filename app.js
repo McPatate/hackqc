6f5cf023908c07f661a2270cf8998bd2ab51417f
Bastien Penavayre <bastienPenava@gmail.com>
2017-03-11 15:34:13 -0500

    Auto update des travaux

--- app.js
+++ app.js
@@ -5,14 +5,57 @@ var mongoose      = require('mongoose');
 var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
+var http          = require('http');
+var xml2js        = require('xml2js');
 
 var index         = require('./routes/index');
 var users         = require('./routes/users');
 var bacs         = require('./routes/bacs');
 
 var db            = require('./config/db');
+var Travaux       = require('./models/travaux');
 
 var app = express();
+
+//insert crone
+var schedule = require('node-schedule');
+
+var j = schedule.scheduleJob('* * * * * *', function(){
+  var parser = new xml2js.Parser();
+  http.get("http://www.quebec511.info/donneespubliques/entraves.ashx?format=xml", function (res)  {
+      if (res.statusCode == 200) {
+          res.setEncoding('utf8');
+          rawData = '';
+          res.on('data', function (chunk) {
+              rawData += chunk
+          });
+          res.on('end', function () {
+            console.log("In END");
+              try {
+                  parser.parseString(rawData, function (err, result) {
+                      var array = [];
+                      result.chantiers.chantier.forEach(function (el) {
+                          array.push({
+                            position: {lat: el.Latitude[0], lon: el.Longitude[0]},
+                              isMaj: (el.EntraveType[0]).startsWith("majeure"),
+                              debut: new Date(el.Debut[0]),
+                              fin: new Date(el.Fin[0])
+                          });
+                      });
+                      Travaux.remove({}, function(err) { if (err) console.error(err); });
+                      Travaux.create(array);
+                  });
+              } catch (e) {
+                  console.error(e);
+              }
+          });
+      }
+
+  }).on('error', function (e)  {
+    console.error(e);
+  });
+});
+
 
 mongoose.connect(db.url);
 
