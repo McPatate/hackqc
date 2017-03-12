5948f6104d616cf0c2fe4ea105afee0f39f2ab95
Bastien Penavayre <bastienPenava@gmail.com>
2017-03-11 19:34:51 -0500

    add geolib

--- routes/travaux.js
+++ routes/travaux.js
@@ -9,36 +9,30 @@ router.route('/')
 
         function distance(lat1, lon1, lat2, lon2)
         {
-            var R = 6371; // Radius of the earth in km
-            var dLat = toRad(lat2-lat1);
-            var dLon = toRad(lon2-lon1);
-            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-                Math.sin(dLon/2) * Math.sin(dLon/2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-            console.log("distance => :", R, a, dLat, dLon, lat1, lon1, lat2, lon2, c, R * c);
-            return R * c; // Distance in km
+            var tmp  = geolib.getDistanceSimple(
+                {latitude: lat1, longitude: lon1},
+                {latitude: lat2, longitude: lon2}
+            );
+            console.log(tmp);
+            return tmp;
         }
 
-        var lat_str = req.query.lat;
-        var lon_str = req.query.lon;
+        var lat = req.query.lat;
+        var lon = req.query.lon;
         var rayon_str = req.query.rayon;
         var check = false;
-        var lat = 0, lon = 0, rayon = 0;
+        var rayon = 0;
 
-        if (lat_str && lon_str && rayon_str) {
+        if (lat && lon && rayon_str) {
             check = true;
-            lat = parseFloat(lat_str);
-            lon = parseFloat(lon_str);
             rayon = parseFloat(rayon_str);
-            console.log("rayon", rayon);
         }
 
         Travaux.find({}, function(err, travs) {
             var travMap = [];
             var i = 0;
             travs.forEach(function(trav) {
-                if (!check || (check && distance(parseFloat(trav.position.lat), parseFloat(trav.position.lon), lat, lon) <= rayon))
+                if (!check || (check && distance(trav.position.lat, trav.position.lon, lat, lon) <= rayon))
                     travMap.push(trav);
             });
             res.status(200).send(travMap);
