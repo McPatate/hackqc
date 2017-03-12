6f5bd51544c434a130c7a95c232e2feb1740134f
Bastien Penavayre <bastienPenava@gmail.com>
2017-03-11 19:41:12 -0500

    correct add geolib

--- routes/travaux.js
+++ routes/travaux.js
@@ -27,15 +27,15 @@ router.route('/')
         if (lat_str && lon_str && rayon_str) {
             check = true;
             rayon = parseFloat(rayon_str);
-            lat = parseInt(lat_str);
-            lon = parseInt(lon_str);
+            lat = parseFloat(lat_str);
+            lon = parseFloat(lon_str);
         }
 
         Travaux.find({}, function(err, travs) {
             var travMap = [];
             var i = 0;
             travs.forEach(function(trav) {
-                if (!check || (check && distance(trav.position.lat, trav.position.lon, parseInt(lat), parseInt(lon)) <= rayon))
+                if (!check || (check && distance(parseFloat(trav.position.lat), parseFloat(trav.position.lon), lat, lon) <= rayon))
                     travMap.push(trav);
             });
             res.status(200).send(travMap);
